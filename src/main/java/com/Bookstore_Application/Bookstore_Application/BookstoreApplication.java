87b7ecfe8594ae3d71018e9fc7f03ec8925a7031
manuraj23 <manuraj082004@gmail.com>
2025-03-29 21:32:12 +0530

    Initial commit

---
+++ src/main/java/com/Bookstore_Application/Bookstore_Application/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.Bookstore_Application.Bookstore_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
