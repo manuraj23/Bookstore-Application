89a36f5700c3c499434faa9f05639b9cff6147a1
manuraj23 <manuraj082004@gmail.com>
2025-03-30 10:45:59 +0530

    Crud Operation for User Completed

--- src/main/java/com/Bookstore_Application/Bookstore_Application/Controller/UserController.java
+++ src/main/java/com/Bookstore_Application/Bookstore_Application/Controller/UserController.java
@@ -1,10 +1,11 @@
 package com.Bookstore_Application.Bookstore_Application.Controller;
 
-import com.Bookstore_Application.Bookstore_Application.Repository.UserRepository;
+import com.Bookstore_Application.Bookstore_Application.Entity.User;
 import com.Bookstore_Application.Bookstore_Application.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -15,6 +16,39 @@ public class UserController {
     private UserService userService;
 
 
+    @PutMapping("/UpdateUser/username/{username}")
+    public ResponseEntity<?>updateUser(@RequestBody User user, @PathVariable String username){
+        try{
+            User userInDb =userService.findByUsername(username);
+            if (userInDb != null) {
+                userInDb.setEmail(user.getEmail());
+                userInDb.setPassword(user.getPassword());
+                userInDb.setUsername(user.getUsername());
+                userService.saveNewUser(userInDb);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
+    @DeleteMapping("/DeleteUser/username/{username}")
+    public ResponseEntity<?>deleteUser(@PathVariable String username){
+        try{
+            User userInDb =userService.findByUsername(username);
+            if (userInDb != null) {
+                userService.deleteUser(userInDb);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
--- src/main/java/com/Bookstore_Application/Bookstore_Application/Service/UserService.java
+++ src/main/java/com/Bookstore_Application/Bookstore_Application/Service/UserService.java
@@ -1,18 +1,24 @@
 package com.Bookstore_Application.Bookstore_Application.Service;
 
+import com.Bookstore_Application.Bookstore_Application.Entity.Books;
 import com.Bookstore_Application.Bookstore_Application.Entity.User;
+import com.Bookstore_Application.Bookstore_Application.Repository.BooksRepsitory;
 import com.Bookstore_Application.Bookstore_Application.Repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class UserService {
 
     @Autowired
     public UserRepository userRepository;
+
+    @Autowired
+    public BooksRepsitory booksRepsitory;
 
     public void saveNewUser(User user) {
         user.setRoles(List.of("USER"));
@@ -30,5 +36,12 @@ public class UserService {
 
     public List<User> getAll() {
         return userRepository.findAll();
+    }
+
+    public void deleteUser(User userInDb) {
+        List<ObjectId> bookIds = userInDb.getBooks().stream().map(Books::getId).toList();
+        userRepository.delete(userInDb);
+        bookIds.forEach(bookId -> booksRepsitory.deleteById(bookId));
+//        System.out.println(bookIds);
     }
 }
